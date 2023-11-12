668084a2dedfefe43848e62c84a051f64e09e4d7
Asim <asimvyl@gmail.com>
2023-11-12 07:05:58 +0530

    post request finished funtion based view

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
 
+from watchlist_app.models import Movie
+
 # we are creting serializer here it maps each individual element here one by one 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only =True)
     name = serializers.CharField()
     description = serializers.CharField()
     active = serializers.BooleanField()
+    
+    
+    # we are creating put request by adding normal function
+    
+    def create(self,validated_data):
+         return Movie.objects.create(**validated_data)
+        
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -4,14 +4,23 @@ from rest_framework.decorators import api_view
 from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializer
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def movie_list(request):
-    movies = Movie.objects.all()
-    # we use many = true because we want to fetch all data becausen we are using function based view 
-    # also have multiple objects here
-    serializer = MovieSerializer(movies,many=True)
-    # we use data method here to fetch all data 
-    return Response(serializer.data)
+    if request.method =='GET':
+        
+        movies = Movie.objects.all()
+        # we use many = true because we want to fetch all data becausen we are using function based view 
+        # also have multiple objects here
+        serializer = MovieSerializer(movies,many=True)
+        # we use data method here to fetch all data 
+        return Response(serializer.data)
+    if request.method =='POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
 
 @api_view(['GET'])
 def movie_details(request,pk):
